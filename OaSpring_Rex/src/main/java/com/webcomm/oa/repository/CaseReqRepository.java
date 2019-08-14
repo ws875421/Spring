8f57e2228c9e2fab5dd94c2c524c2061530e0ed1
Rex <ws0936167890@gmail.com>
2019-08-14 16:14:49 +0800

    JPQL&Native

--- OaSpring_Rex/src/main/java/com/webcomm/oa/repository/CaseReqRepository.java
+++ OaSpring_Rex/src/main/java/com/webcomm/oa/repository/CaseReqRepository.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Repository;
 import com.webcomm.oa.domain.CaseReq;
 
 @Repository
-public interface CaseReqRepository extends JpaRepository<CaseReq, String>, JpaSpecificationExecutor<CaseReq>,CrudRepository<CaseReq, String> {
-	
-	@Modifying
-	@Query("delete CaseReq where CASE_NO in ?1")
-	public int deleteCaseNos(String[] caseNos);
+public interface CaseReqRepository extends JpaRepository<CaseReq, String>, JpaSpecificationExecutor<CaseReq>, CrudRepository<CaseReq, String> {
 
+//	@Modifying//JPQL
+//	@Query("delete CaseReq where CASE_NO in ?1")
+//	public int deleteCaseNos(String[] caseNos);
+
+	@Modifying // Native
+	@Query(value = "delete CASE_REQ where CASE_NO in ?1", nativeQuery = true)
+	public int deleteCaseNos(String[] caseNos);
 }
