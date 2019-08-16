c5df2709380ad50ac268905466d10cb78e8b2f50
Rex <ws0936167890@gmail.com>
2019-08-16 11:35:25 +0800

    @Param("caseNos")

--- OaSpring_Rex/src/main/java/com/webcomm/oa/repository/CaseReqRepository.java
+++ OaSpring_Rex/src/main/java/com/webcomm/oa/repository/CaseReqRepository.java
@@ -10,17 +10,23 @@ import org.springframework.stereotype.Repository;
 import com.webcomm.oa.domain.CaseReq;
 
 @Repository
-public interface CaseReqRepository extends JpaRepository<CaseReq, String>, JpaSpecificationExecutor<CaseReq>, CrudRepository<CaseReq, String> {
+public interface CaseReqRepository
+		extends JpaRepository<CaseReq, String>, JpaSpecificationExecutor<CaseReq>, CrudRepository<CaseReq, String> {
 
 //	@Modifying//JPQL
 //	@Query("delete CaseReq where CASE_NO in ?1")
 //	public int deleteCaseNos(String[] caseNos);
 
-	@Modifying // Native
-	@Query(value = "delete CASE_REQ where CASE_NO in ?1", nativeQuery = true)
-	public int deleteCaseNos(String[] caseNos);
-	
+//	@Modifying // Native
+//	@Query(value = "delete CASE_REQ where CASE_NO in ?1", nativeQuery = true)
+//	public int deleteCaseNos(String[] caseNos);
+
 //	@Modifying // Native
 //	@Query(value = "delete CaseReq where CASE_NO in ?1", nativeQuery = false)
 //	public int deleteCaseNos(String[] caseNos);
+
+	@Modifying // Native
+	@Query(value = "delete CASE_REQ where CASE_NO in :caseNos", nativeQuery = true)
+	public int deleteCaseNos(@Param("caseNos") String[] caseNos);
+
 }
